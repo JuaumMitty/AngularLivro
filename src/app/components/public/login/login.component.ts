6194adf5b2d60c64f78b233ff62c3783d8df7928
JuaumMitty <jjoaovictorpg@gmail.com>
2021-10-17 22:07:19 -0300

    Componente login

--- src/app/components/public/login/login.component.ts
+++ src/app/components/public/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email!: string;
+  senha!: string;
+  mensagem!: string;
+  emailEnviado!: boolean;
+
+  constructor(private authServ: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logar(){
+    try {
+      if(this.email == undefined || this.senha == undefined) {
+        this.mensagem = 'Usuário ou senha vazios'
+        return
+      } else {
+        this.authServ.login(this.email, this.senha).then(() => {
+          this.router.navigate(['/admin/painel']);
+        }
+        ).catch( erro => {
+          let detalhes = '';
+          switch(erro.code){
+            case 'auth/user-not-found': {
+              detalhes = 'Não existe usuário para o e-mail informado';
+              break;
+            }
+            case 'auth/invalid-email': {
+              detalhes = 'E-mail inválido';
+              break;
+            }
+            case 'auth/wrong-password': {
+              detalhes = 'Senha inválida';
+              break;
+            }
+            default: {
+              detalhes = erro.message;
+              break;
+            }
+          }
+          this.mensagem = `Erro ao logar. ${detalhes}`;
+        });
+      }
+    } catch (erro) {
+      this.mensagem = `Erro ao logar. Detalhes: ${erro}`;
+    }
+  }
+
+  async enviaLink(){
+    const { value: email } = await Swal.fire({
+      title: 'Informe o email cadastrado',
+      input: 'email',
+      inputPlaceholder: 'email'
+    })
+  }
 }
